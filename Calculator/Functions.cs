75ce7a859d4668a5bc8dc9ad1cd977775683b1c9
Okja88 <kerhl8@gmail.com>
2021-03-05 15:31:37 +0800

    Demonstration of OOP and constructor

---
+++ Calculator/Functions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class Functions
+    {
+        //Fields
+        private double _num1;
+        private double _num2;
+        public Functions(double a,double b) //constructor
+
+        {
+            _num1 = a;
+            _num2 = b;
+        }
+        /// <summary>
+        /// This function adds 2 double numbers.
+        /// <para>It returns a string</para>
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string Add(double a, double b) //doesnt return anything, hence put void, functions r to be Added, there are two parameters, no semicolon needed 
+        {
+            return $"The sum of {a} and {b} is {Math.Round(a + b, 2)}. \n And the values of _num1 and _num2 are {_num2} and {_num2}."; // rounding to 2 decima places
+                                                                         //return Math.Round(a + b, 2); //returns a double data type, note the double in Add, instead of void
+        }
+        /// <summary>
+        /// This function subtracts 2 double numbers.
+        /// <para>It returns a string</para>
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public string Subtract(double a, double b) //doesnt return anything, hence put void, functions r to be Added, there are two parameters, no semicolon needed 
+        {
+            return $"The sum of {a} and {b} is {Math.Round(a - b, 2)}."; // rounding to 2 decima places
+                                                                         //return Math.Round(a + b, 2); //returns a double data type, note the double in Add, instead of void
+
+        }
+    }
+} // "Static" is removed from all Functions.cs but remained in program.cs. But for program.cs, "Static" remained in program.cs as there is a need for other classes to call this class in program
+
+
+
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -16,13 +16,32 @@ namespace Calculator
             Console.WriteLine("Please enter the second number:");
             string number2 = Console.ReadLine();
 
-            Add(double.Parse(number1), double.Parse(number2)); //add (1100,123130);
-        }
-        public static void Add(double a, double b) //doesnt return anything, hence put void, functions r to be Added, there are two parameters
+            string result = Add(double.Parse(number1), double.Parse(number2)); // these two lines 19 & 20 are alternative to line 22
+                Console.WriteLine(result);
+
+            //Console.WriteLine(Add(double.Parse(number1), double.Parse(number2))); // put in Console.WriteLine
+
+            Functions myFunctions = new Functions(9,10); //instantiating a new functions called myfunctions, note the "new"
+            Console.WriteLine(myFunctions.Add(2, 3));
+            Console.WriteLine(myFunctions.Subtract(2, 4));
+
+            Functions myFunctions2 = new Functions(11,12); //instantiating a new functions called myfunctions, note the "new"
+            Console.WriteLine(myFunctions2.Add(5, 10));
+            Console.WriteLine(myFunctions2.Subtract(4, 10));
+
+        }/// <summary>
+         /// This function adds 2 double numbers.
+         /// <para>It returns a string</para>
+         /// </summary>
+         /// <param name="a"></param>
+         /// <param name="b"></param>
+         /// <returns></returns>
+        public static string Add(double a, double b) //doesnt return anything, hence put void, functions r to be Added, there are two parameters, no semicolon needed 
         {
-            Console.WriteLine($"The sum of {a} and {b} is {a + b}");
-                
-            
+            return $"The sum of {a} and {b} is {Math.Round(a + b, 2)}."; // rounding to 2 decima places
+            //return Math.Round(a + b, 2); //returns a double data type, note the double in Add, instead of void
+
+
         }
     }
-}
+}//string the reult as return, and string the Add(double a, double b) in line 24
