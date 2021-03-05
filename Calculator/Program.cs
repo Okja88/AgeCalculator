59490fdcd0409f8e5494b679a630a7d2763d5d56
Okja88 <kerhl8@gmail.com>
2021-03-05 11:00:16 +0800

    Function with two parameters & user input
    Create a function with two parameters, and user input where inputs are parsed as double

---
+++ Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add(1, 2); //data in brackets are arguments/parameters
+            //Add(333, 222);
+            //Add(double.Parse("2"), 2);
+
+            Console.WriteLine("Please enter the first number:");
+            string number1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter the second number:");
+            string number2 = Console.ReadLine();
+
+            Add(double.Parse(number1), double.Parse(number2)); //add (1100,123130);
+        }
+        public static void Add(double a, double b) //doesnt return anything, hence put void, functions r to be Added, there are two parameters
+        {
+            Console.WriteLine($"The sum of {a} and {b} is {a + b}");
+                
+            
+        }
+    }
+}
